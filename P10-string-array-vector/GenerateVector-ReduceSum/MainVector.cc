5c88fcd619dd663ad9d2cf0c435d1af1a33d3545
root <root@DESKTOP-HTLDHTU>
2023-11-14 18:47:43 +0000

    Práctica IB

---
+++ P10-string-array-vector/GenerateVector-ReduceSum/MainVector.cc
@@ -0,0 +1,54 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Informática básica
+ *
+ * @author Alfredo
+ * @date 14/11/2023
+ * @brief este programa devuelve un vector de double aleatorios
+ * @see https://google.github.io/styleguide/cppguide.html
+ *
+ */
+
+#include "Vector.h"
+
+int main() {
+  // FASE I
+  Vector vector {0, 0.f, 0.f};
+  vector.EnterLowerUpperSize();
+  std::vector<double> my_vector = vector.GenerateVector();
+  std::cout << "El vector generado es ";
+  vector.ReadVector(my_vector);
+  std::cout << "El vector ordenado es ";
+  vector.SortVector(my_vector);
+  //  std::cout << "El vector sin repetidos es ";
+  //  vector.UniqueVector(my_vector);
+
+  // FASE II
+  std::cout << "La suma de los elementos del vector es: ";
+  vector.SumVector(my_vector);
+
+  // FASE III
+  vector.MMAVector(my_vector);
+  // std::cout << "El valor maximo del vector es: ";
+  // vector.MaximunValue(my_vector);
+  // std::cout << "El valor minimo del vector es: ";
+  // vector.MinimunValue(my_vector);
+  // std::cout << "El valor medio del vector es: ";
+  // vector.AverageValue(my_vector);
+
+  // FASE IV
+  Vector vector2 {0, 0.f, 0.f};
+  std::cout << "Introduzca el tamaño, valor minimo y valor maximo del segundo vector \n";
+  vector2.EnterLowerUpperSize();
+  std::vector<double> my_vector2 = vector2.GenerateVector();
+  std::cout << "El vector generado es ";
+  vector.ReadVector(my_vector2);
+  std::cout << "El vector ordenado es ";
+  vector.SortVector(my_vector2);
+  std::vector<double> my_vector3 = vector.ConcatenateVectors(my_vector, my_vector2);
+  std::cout << "El vector concatenado es ";
+  vector.ReadVector(my_vector3);
+  return 0;
+}
---
+++ P10-string-array-vector/GenerateVector-ReduceSum/Vector.cc
@@ -0,0 +1,129 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Informática básica
+ *
+ * @author Alfredo
+ * @date 14/11/2023
+ * @brief este programa devuelve un vector de double aleatorios
+ * @see https://google.github.io/styleguide/cppguide.html
+ *
+ */
+
+#include "Vector.h"
+
+Vector::Vector(const int& size, const double& lower, const double& upper) {
+  size_ = size;
+  lower_ = lower;
+  upper_ = upper;
+}
+
+// FASE I
+std::vector<double> Vector::GenerateVector() {
+  std::vector<double> vector {};
+  vector.reserve(this->get_size());
+  std::default_random_engine generator;
+  std::uniform_real_distribution<double> distribution(this->get_lower(),this->get_upper());
+  for (int i = 0; i < this->get_size(); i++) {
+    vector.push_back(distribution(generator));
+  }
+  std::shuffle(vector.begin(), vector.end(), generator); // Shuffle the vector
+  return vector;
+}  
+
+void Vector::ReadVector(const std::vector<double>& vector) {
+  std::cout << "{ ";
+  for (const auto& value : vector) {
+    std::cout << value << " ";
+  }
+  std::cout << "}\n";
+}
+
+void Vector::SortVector(const std::vector<double>& vector) {
+  std::vector<double> sorted_vector = vector;
+  std::sort(sorted_vector.begin(), sorted_vector.end());
+  this->ReadVector(sorted_vector);
+}
+
+void Vector::UniqueVector(const std::vector<double>& vector) {
+  std::vector<double> unique_vector = vector;
+  std::sort(unique_vector.begin(), unique_vector.end());
+  unique_vector.erase(std::unique(unique_vector.begin(), unique_vector.end()), unique_vector.end());
+  this->ReadVector(unique_vector);
+}
+
+// FASE II
+void Vector::SumVector(const std::vector<double>& vector) {
+  double result {0.0}; 
+  for(size_t i {0}; i < vector.size(); ++i) {
+    result += vector.at(i);
+  } 
+  std::cout << result << std::endl;
+}
+
+// FASE III
+void Vector::MaximunValue(const std::vector<double>& vector) {
+  std::vector<double> sorted_vector = vector;
+  std::sort(sorted_vector.begin(), sorted_vector.end());
+  std::cout << sorted_vector.back() << std::endl;
+}
+
+void Vector::MinimunValue(const std::vector<double>& vector) {
+  std::vector<double> sorted_vector = vector;
+  std::sort(sorted_vector.begin(), sorted_vector.end());
+  std::cout << sorted_vector.front() << std::endl;
+}
+
+void Vector::AverageValue(const std::vector<double>& vector) {
+  double sum = std::accumulate(vector.begin(), vector.end(), 0.0);
+  double average = sum / vector.size();
+  std::cout << average << std::endl;
+}
+
+void Vector::MMAVector(std::vector<double>& vector) {
+  std::vector<double> sorted_vector = vector;
+  std::sort(sorted_vector.begin(), sorted_vector.end());
+  std::cout << "El valor maximo del vector es: " << sorted_vector.back() << std::endl;
+  std::cout << "El valor minimo del vector es: " << sorted_vector.front() << std::endl;
+  double sum = std::accumulate(vector.begin(), vector.end(), 0.0);
+  double average = sum / vector.size();
+  std::cout << "El valor medio del vector es: " << average << std::endl;
+}
+
+// FASE IV
+std::vector<double> Vector::ConcatenateVectors(const std::vector<double>& vector1, const std::vector<double>& vector2) {
+  std::vector<double> vector3 {};
+  vector3.reserve(vector1.size() + vector2.size());
+  vector3.insert(vector3.end(), vector1.begin(), vector1.end());
+  vector3.insert(vector3.end(), vector2.begin(), vector2.end());
+  return vector3;
+}
+
+// E/R
+void Vector::EnterLowerUpperSize(std::istream& is){
+  std::cout << "Introduzca el tamaño del vector: ";
+  is >> size_;
+  std::cout << "Introduzca el limite inferior: ";
+  is >> lower_;
+  std::cout << "Introduzca el limite superior: ";
+  is >> upper_;
+}
+
+void Vector::ReadLowerUpperSize(std::ostream& os){
+  os << "El tamaño del vector es: " << this->get_size() << std::endl;
+  os << "El limite inferior es: " << this->get_lower() << std::endl;
+  os << "El limite superior es: " << this->get_upper() << std::endl;
+}
+
+// std::vector<double> Vector::GenerateVector() {
+//   std::vector<double> vector {};
+//   vector.resize(this->get_size());
+//   vector.reserve(this->get_size());
+//   std::default_random_engine generator;
+//   std::uniform_real_distribution<double> distribution(this->get_lower(),this->get_upper());
+//   for (int i = 0; i < this->get_size(); i++) {
+//     vector[i] = distribution(generator);
+//   }
+//   return vector;
+// }  
